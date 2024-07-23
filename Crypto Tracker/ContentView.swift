8fe32771f21ad1fe11bc6d2bca0ba7513ac60741
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2024-07-23 18:07:09 +0900

    added assets folder

---
+++ Crypto Tracker/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Crypto Tracker
+//
+//  Created by Hlwan Aung Phyo on 2024/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto Tracker/Crypto Tracker/Extensions/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  Crypto Tracker
+//
+//  Created by Hlwan Aung Phyo on 2024/07/23.
+//
+
+import Foundation
